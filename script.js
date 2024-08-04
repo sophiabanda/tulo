e3974f91ded95d291aebd0114f3c3db97ff21357
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-04 16:57:40 -0400

    Shaped, Pseudo code

---
+++ script.js
@@ -0,0 +1,14 @@
+/*
+- there are two of each shape, labeled with classname
+- total of 12 shapes
+- if click 1 matches click 2, it's a match!
+- else, try again to match click 1 and 2
+- when all matches are made, you win
+- celebration!
+*/
+
+// CONTSTANTS
+// STATE VARIABLES
+// CACHED ELEMENTS
+// EVENT LISTENERS
+// FUNCTIONS
