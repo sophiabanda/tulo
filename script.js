8c2eae3df12334015368050e3e7dcc546e089b9d
Sophia M Banda <51414936+sophiabanda@users.noreply.github.com>
2024-08-10 16:25:21 -0400

    Recreate shapes WIP

--- script.js
+++ script.js
@@ -10,28 +10,7 @@
 
 // CONTSTANTS
 
-const shapesArray = [
-  `<path class="pentagon" d="M 100 10 L 190 80 L 160 190 L 40 190 L 10 80" />`,
-  `<path class="rhombus" d="M 100 10 L 160 100 L 100 190 L 40 100 z" />`,
-  `<path class="hexagon" d="M 50 20 L 150 20 L 190 100 L 150 180 L 50 180 L 10 100 z" />`,
-  `<path class="parallelogram" d="M 80 10 L 200 10 L 120 150 L 0 150 z" />`,
-  `<path class="trapezoid" d="M 45 10 L 125 10 L 175 125 L 0 125 Z" />`,
-  `<path class="triangle" d="M 100 0 L 200 150 L 0 150 L 100 0" />`,
-  `<rect class="rectangle" x="0" y="0" width="200" height="110" />`,
-  `<rect class="square" x="25" y="0" width="130" height="130" />`,
-  `<ellipse class="oval" cx="100" cy="100" rx="100" ry="50" />`,
-  `<path class="octagon" d="M 66.66 20 L 133.33 20 L 180 66.66 L 180 133.33 L 180 133.33 L 133.33 180 L 66.66 180 L 20 133.33 L 20 66.66 z" />`,
-  `<path class="pentagon" d="M 100 10 L 190 80 L 160 190 L 40 190 L 10 80" />`,
-  `<path class="rhombus" d="M 100 10 L 160 100 L 100 190 L 40 100 z" />`,
-  `<path class="hexagon" d="M 50 20 L 150 20 L 190 100 L 150 180 L 50 180 L 10 100 z" />`,
-  `<path class="parallelogram" d="M 80 10 L 200 10 L 120 150 L 0 150 z" />`,
-  `<path class="trapezoid" d="M 45 10 L 125 10 L 175 125 L 0 125 Z" />`,
-  `<path class="triangle" d="M 100 0 L 200 150 L 0 150 L 100 0" />`,
-  `<rect class="rectangle" x="0" y="0" width="200" height="110" />`,
-  `<rect class="square" x="25" y="0" width="130" height="130" />`,
-  `<ellipse class="oval" cx="100" cy="100" rx="100" ry="50" />`,
-  `<path class="octagon" d="M 66.66 20 L 133.33 20 L 180 66.66 L 180 133.33 L 180 133.33 L 133.33 180 L 66.66 180 L 20 133.33 L 20 66.66 z" />`,
-];
+const shapesArray = [];
 const colorOptions = [
   'red',
   'orange',
@@ -65,19 +44,7 @@ const svgDivs = document.querySelectorAll('svg');
 
 initialize();
 
-function initialize() {
-  const shuffledShapes = shuffleShapes();
-  const shuffledColors = shuffleColors();
-
-  svgDivs.forEach((svg, index) => {
-    svg.innerHTML = shuffledShapes[index];
-
-    const shapeElement = svg.querySelector('path, rect, ellipse');
-    if (shapeElement) {
-      shapeElement.setAttribute('fill', shuffledColors[index]);
-    }
-  });
-}
+function initialize() {}
 
 function shuffleShapes() {
   const shuffArray = shapesArray.slice();
